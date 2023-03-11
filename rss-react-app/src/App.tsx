d7f241a3c383a266c1b16e4550aed8e9491b31d6
Mihail <rhbkickshot@gmail.com>
2023-03-11 22:49:36 +0300

    feat: add react-router

--- rss-react-app/src/App.tsx
+++ rss-react-app/src/App.tsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div className="wrapper">
+        <Routes></Routes>
+      </div>
+    );
   }
 }
